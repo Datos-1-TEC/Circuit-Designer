a7392f1f5945b8e8b18638a7ef91c97916947085
igmochang <ig.morales@estudiantec.cr>
2020-12-31 18:04:04 -0600

    [Update] GUI_CircuitDesigner

--- GUI_CircuitDesigner.py
+++ GUI_CircuitDesigner.py
@@ -1,8 +1,36 @@
-from tkinter import *
+try:
+    import Tkinter as tk     ## Python 2.x
+except ImportError:
+    import tkinter as tk     ## Python 3.x
 
-root = Tk()
-root.title('Circuit Designer')
-root.geometry('1000x640')
-root.resizable(width = NO, height=NO)
+class SideBar():
+    def __init__(self, root):
+        sidebarframe = tk.Frame(root)
+        self.sometext = tk.Text(sidebarframe)
+        button1 = tk.Button(sidebarframe, text="do something",
+                          command = self.do_something)
+        
+        button2 = tk.Button(sidebarframe, text="do something",
+                          command = self.do_something)
 
-root.mainloop()
+        sidebarframe.grid(row = 0, column = 0)
+        self.sometext.grid(row = 0, column = 1)
+        button1.grid(row = 0, column = 2)
+        button2.grid(row = 0, column = 3)
+
+        self.sometext.focus_set()
+
+    def do_something(self):
+        self.sometext.delete(1.0, "end")
+        print ("do something")
+
+class MainApplication():
+    def __init__(self, parent):
+        self.parent = parent
+        self.SB = SideBar(self.parent)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    MainApplication(root)
+    root.mainloop()
