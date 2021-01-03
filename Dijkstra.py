16bb423553dd455e55e25f006e956883220c2524
maicolvn1399 <55199282+maicolvn1399@users.noreply.github.com>
2021-01-02 19:46:30 -0600

    [Update] Dijsktra algorithm

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,4 +1,6 @@
 from Graph import Graph, Node
+from LinkedList import LinkedList
+import sys
 
 class Dijkstra:
     def __init__(self):
@@ -12,8 +14,68 @@ class Dijkstra:
 
         self.unsettledNodes.add(source)
         while(len(self.unsettledNodes)!=0):
-            currentNode = getLowestDistanceNode(self.unsettledNodes)
+            adjacencyPair = {}
+            #currentNode = Node("")
+            currentNode = self.getLowestDistanceNode(self.unsettledNodes)
             self.unsettledNodes.remove(currentNode)
+            for n,i in currentNode.get_adjacent_nodes():
+                adjacencyPair[n] = i
+                adjacentNode = adjacencyPair.get(n)
+                edgeWeight = adjacencyPair.get(i)
+                if not self.settledNodes.__contains__(adjacentNode):
+                    calculateMinimumDistance(adjacentNode,edgeWeight,currentNode)
+                    self.unsettledNodes.add(adjacentNode)
+
+            self.settledNodes.add(currentNode)
+
+        return graph
+
+
+    def getLowestDistanceNode(self, unsettledNodes):
+        lowestDistanceNode = None
+        lowestDistance = sys.maxsize
+        for node in unsettledNodes:
+            nodeDistance = node.get_distance()
+            if nodeDistance < lowestDistance:
+                lowestDistance = nodeDistance
+                lowestDistanceNode = node
+
+        return lowestDistanceNode
+
+    def calculateMinimumDistance(self, evaluationNode, edgeWeight, sourceNode):
+        sourceDistance = sourceNode.get_distance()
+        if sourceDistance + edgeWeight < evaluationNode.get_distance():
+            evaluationNode.set_distance(sourceNode + edgeWeight)
+            shortestPath = sourceNode.get_shortest_path()
+            shortestPath.insert(sourceNode)
+            evaluationNode.set_shortest_path(shortestPath)
+            
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
 
 
 
