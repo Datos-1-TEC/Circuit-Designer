4679f51f3b8ab94707361493ce6ea29a3183f2c3
Maxg2212 <maxgarro22@gmail.com>
2021-01-03 18:32:58 -0600

    [Update] GUI

--- GUI_CircuitDesigner.py
+++ GUI_CircuitDesigner.py
@@ -47,6 +47,7 @@ class popupWindow(object):
         self.e2.pack()
         self.b = Button(top, text = 'Ok', command = self.accept)
         self.b.pack()
+
     
     def accept(self):
         MA.SB.createResistor(self.e2.get(), self.e.get())
@@ -54,7 +55,31 @@ class popupWindow(object):
 
     def cleanup(self):
         self.value = self.e.get()
-        self.top.destroy()        
+        self.top.destroy()      
+
+    
+
+class popupWindowVol(object):
+    def __init__(self, master):
+        top = self.top = Toplevel(master)
+        self.l3 = Label(top,text="Inserte un nombre para la fuente de voltaje")
+        self.l3.pack()
+        self.e3 = Entry(top)
+        self.e3.pack()
+        self.l4 = Label(top,text="Inserte un valor para la fuente de voltaje")
+        self.l4.pack()
+        self.e4 = Entry(top)
+        self.e4.pack()
+        self.b2 = Button(top, text = 'Ok', command = self.accept2)
+        self.b2.pack()
+
+    def accept2(self):
+        MA.SB.createFuenteVoltaje(self.e4.get(), self.e3.get())
+        self.cleanup2()
+
+    def cleanup2(self):
+        self.value = self.e3.get()
+        self.top.destroy()  
 
 class SideBar():
     def cargarimg(self, archivo): # Se carga imagen
@@ -68,7 +93,7 @@ class SideBar():
         self.resBut = Button(self.window, image = resImage, command = self.nameRes)        
         self.resBut.image = resImage
         self.resBut.place(anchor = CENTER, x = 100, y = 200)
-        self.volBut = Button(self.window, text = "Fuente", image = volImage)
+        self.volBut = Button(self.window, text = "Fuente", image = volImage, command = self.nameVol)
         self.volBut.image = volImage
         self.volBut.place(anchor = CENTER, x = 100, y = 400)
         
@@ -82,7 +107,17 @@ class SideBar():
         Res1 = Resistor(self.root, value, name)
         print(Res1.name)
         print(Res1.resistance)
-        
+
+    def nameVol(self):
+        self.w = popupWindowVol(self.root)
+        self.volBut["state"] = "disabled" 
+        self.root.wait_window(self.w.top)
+        self.volBut["state"] = "normal"
+
+    def createFuenteVoltaje(self, value, name):       
+        Vol1 = FuenteVoltaje(self.root, value, name)
+        print(Vol1.name)
+        print(Vol1.voltage)    
 
     def __init__(self, root):
         self.root = root
@@ -123,6 +158,34 @@ class Resistor():
         self.show_res()
         MA.MP.paintWindow.bind('<B1-Motion>', self.move)
         
+
+class FuenteVoltaje():
+    def cargarimg(self, archivo): # Se carga imagen
+        ruta = os.path.join('img', archivo)
+        imagen = PhotoImage(file = ruta)
+        return imagen
+
+    def show_vol(self):
+        volImage = self.cargarimg('FuenteVoltaje.png')
+        MA.MP.paintWindow.image = volImage
+        MA.MP.paintWindow.create_image(100, 100, image = volImage)
+        MA.MP.paintWindow.create_line(0,0,100,100)
+        print("Se puso la fuente de voltaje")
+
+    def move(self, e):
+        volImage = self.cargarimg('FuenteVoltaje.png')
+        MA.MP.paintWindow.image = volImage
+        MA.MP.paintWindow.create_image(e.x, e.y, image = volImage)
+    
+    
+    def __init__(self, root, voltage, name):
+        self.root = root
+        self.voltage = voltage
+        self.name = name
+        self.x = 50
+        self.y = 50
+        self.show_vol()
+        MA.MP.paintWindow.bind('<B1-Motion>', self.move)
 
 
 class MainApplication():
