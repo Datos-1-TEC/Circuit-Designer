09b2454ce30faf18b7401e86af0fc4e62bd1ad17
juanprrr <54974335+juanprrr@users.noreply.github.com>
2021-01-03 23:24:50 -0600

    [Created] Resistor, Voltage source and Circuit example implementation

--- Graph.py
+++ Graph.py
@@ -46,10 +46,23 @@ class Node:
 
 class Graph:
     def __init__(self):
+
         self.nodes = set()
+        self.edges  = {}
 
     def addNode(self,node):
         self.nodes.add(node)
+
+    def add_edge(self, start, end, distance):   
+        if start not in self.edges:
+            self.edges[start] = {}
+
+        self.edges[start][end] = distance
+
+    def print_edges(self):
+        edges = self.edges
+        for key, value in edges.items():
+            print(key.get_name(), ' : ', value)
 
     def print_graph_nodes(self):
         printedSet = []
@@ -57,9 +70,120 @@ class Graph:
             printedSet += [x.get_name()]
         print(printedSet)
 
+class Resistor:
+    def __init__(self, name, val): 
+        self.name = name
+        self.val = val
+        self.prev_node = Node("PREV")
+        self.next_node = Node("NEXT")
+        self.unit = "Ohms"
+
+    def get_name(self):
+        return self.name
+
+    def get_val(self):
+        return self.val
+    
+    def set_name(self, name):
+        self.name = name
+    
+    def set_val(self, val):
+        self.val = val
+
+    def get_prev_node(self):
+        return self.prev_node
+    
+    def get_next_node(self):
+        return self.next_node
+    
+    def set_next_node(self, connection):
+        self.next_node = connection
+
+    def set_prev_node(self, connection):
+        self.prev_node = connection
+
+    def print_resistor(self):
+        print("Resistor name: %s value: %s", self.get_name(), self.get_val(), self.unit)
+
+class Voltage:
+    def __init__(self, name, val): 
+        self.name = name
+        self.val = val
+        self.prev_node = Node("PREV")
+        self.next_node = Node("NEXT")
+        self.reference = 0
+        self.unit = "V"
+
+    def get_name(self):
+        return self.name
+
+    def get_val(self):
+        return self.val
+    
+    def get_reference(self):
+        return self.reference
+    
+    def set_name(self, name):
+        self.name = name
+    
+    def set_val(self, val):
+        self.val = val
+
+    def get_prev_node(self):
+        return self.prev_node
+    
+    def get_next_node(self):
+        return self.next_node
+
+    def print_voltage(self):
+        print("Source name: %s value: %s", self.get_name(), self.get_val(), self.unit)
+
+class CircuitExample:
+    def __init__(self):
+        self.name = "Ejemplo de circuito"
+        self.resistorsList = LinkedList()
+        self.voltSources = LinkedList()
+        self.create_components()
+        
+    
+    def create_components(self):
+
+        R1  = Resistor("R1", 100)
+        R2  = Resistor("R2", 200)
+        R3  = Resistor("R3", 300)
+        V1 = Voltage("Source", 5)
+
+        V1.get_prev_node().set_name("0")
+        V1.get_next_node().set_name("1")
+
+        R1.set_prev_node(V1.get_next_node())
+        R1.get_next_node().set_name("2")
+
+        R2.set_prev_node(R1.get_next_node())
+        R2.set_next_node(V1.get_prev_node())
+
+        R3.set_prev_node(R2.get_prev_node())
+        R3.set_next_node(V1.get_prev_node())
+
+        circuitGraph = Graph()
+        circuitGraph.addNode(R1.get_prev_node())
+        circuitGraph.addNode(R1.get_next_node())
+        circuitGraph.addNode(R2.get_next_node())
+
+        circuitGraph.add_edge(R1.get_prev_node(), R1.get_next_node(), R1.get_val())
+        circuitGraph.add_edge(R2.get_prev_node(), R2.get_next_node(), R2.get_val())
+        circuitGraph.add_edge(R3.get_prev_node(), R3.get_next_node(), R3.get_val())
+
+        circuitGraph.print_edges()
 
 
-nodeA = Node("A")
+C1 = CircuitExample()
+
+
+
+
+
+""" nodeA = Node("A")
 nodeB = Node("B")
 nodeC = Node("C")
 nodeD = Node("D")
@@ -81,9 +205,9 @@ nodeF.add_destination(nodeE,5)
 
 
 print("method get_adjacent_nodes()")
-print(nodeA.get_adjacent_nodes())
-print(nodeA.get_distance())
-print(nodeA.get_name())
+print(nodeB.get_adjacent_nodes())
+print(nodeB.get_distance())
+print(nodeB.get_name()) 
 
 
 
@@ -99,12 +223,28 @@ graph.addNode(nodeD)
 graph.addNode(nodeE)
 graph.addNode(nodeF)
 
+graph.add_edge(nodeA, nodeB, 10)
+graph.add_edge(nodeA, nodeC, 15)
+
+graph.add_edge(nodeB, nodeD, 12)
+graph.add_edge(nodeB, nodeF, 15)
+
+graph.add_edge(nodeC, nodeE, 10)
+
+graph.add_edge(nodeD, nodeE, 2)
+graph.add_edge(nodeD, nodeF, 1)
+
+graph.add_edge(nodeF, nodeE, 5)
+
+graph.print_edges()
+
+
+
+
 #graph.print_graph_nodes()
 
 
-
-
-print("done")
+print("done") """
         
 
         
