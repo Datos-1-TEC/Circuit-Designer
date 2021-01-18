78efd255dab396b789c187166ccfdd584b6ba601
maicolvn1399 <55199282+maicolvn1399@users.noreply.github.com>
2021-01-18 17:08:08 -0600

    Revert "[Update] netlist importer class updated"
    This reverts commit 16bc306bd3b05d4c7d6f039cdc1e8659c6de1852.

--- src/GUI_CircuitDesigner.py
+++ src/GUI_CircuitDesigner.py
@@ -9,7 +9,6 @@ from Graph import *
 from ElectricCircuit import *
 from CanvasToolTip import *
 from NetlistExporter import *
-from NetlistImporter import *
 from tkinter import filedialog 
 from Dijkstra2 import * 
 Simulating = False
@@ -38,18 +37,12 @@ class DropDown():
 
     def import_netlist(self):
         print("Import netlist file")
-        netlist_file = filedialog.askopenfilename(filetypes = (("Netlist Files", "*.txt")
+        netlist_file = filedialog.askopenfile(filetypes = (("Netlist Files", "*.txt")
                                                          ,("All files", "*.*") ))
-
         print(netlist_file)
 
-        netlist_importer = NetlistImporter(netlist_file)
-        netlist_importer.get_electric_components_from_file()
-
-
-
-
-
+    #def getsimulating(self):
+        #return self.simulating
 
     def test_dict(self):
         graph = MA.ElectricCircuit.get_graph_as_dict()
--- src/NetlistImporter.py
+++ src/NetlistImporter.py
@@ -2,19 +2,5 @@
 class NetlistImporter:
     def __init__(self,file):
         self.file = file
-        self.splited_list = self.file.split("/")
-        self.file_name = self.splited_list[len(self.splited_list)-1]
-        self.electric_components_list = []
-
-    def get_electric_components_from_file(self):
-        self.file = open(self.file_name, "r")
-        self.lines = self.file.readlines()
-
-        for line in self.lines:
-            self.electric_components_list += [line]
-            print(line)
-
-        print("Returned list")
-        print(self.electric_components_list)
         
         
